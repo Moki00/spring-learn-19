032142f2d43aa3a6276ad0e093be0bb633c68f10
MoKi <Mokiwins@gmail.com>
2023-04-19 15:54:47 -0400

    8080 works

--- src/main/java/com/simplilearn/SpringLearnApplication.java
+++ src/main/java/com/simplilearn/SpringLearnApplication.java
@@ -2,12 +2,22 @@ package com.simplilearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name1", defaultValue = "Class") String name) {
+		return name;
+
 	}
 
 }
